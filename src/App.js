0f0b0e08f095db7b40468b00374d693300f40c04
jeevan <jkc5186@gmail.com>
2021-12-11 00:54:41 +0545

    completion of header component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import {  Routes , Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homePage/HomePage';
 import ShopPage from './pages/shopPage/ShopPage';
-
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div>
+        <Header/>
       	<Routes>
           <Route exact path='/' element={<HomePage/>} />
           <Route path='/shop' element={<ShopPage/>} />
